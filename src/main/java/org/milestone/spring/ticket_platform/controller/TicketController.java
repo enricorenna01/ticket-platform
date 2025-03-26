3dad3b15155ebe10ca1976547eaf7f1815e716c6
enricorenna01 <erenna39@gmail.com>
2025-03-26 16:40:10 +0100

    feat: added autowired

--- src/main/java/org/milestone/spring/ticket_platform/controller/TicketController.java
+++ src/main/java/org/milestone/spring/ticket_platform/controller/TicketController.java
@@ -1,6 +1,12 @@
 package org.milestone.spring.ticket_platform.controller;
 
+import java.util.List;
+
+import org.milestone.spring.ticket_platform.model.Ticket;
+import org.milestone.spring.ticket_platform.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,8 +15,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/ticket")
 public class TicketController {
     
+    @Autowired
+    private TicketRepository repository;
+
     @GetMapping()
-    public String index() {
+    public String index(Model model) {
+        List<Ticket> tickets = repository.findAll();
+        model.addAttribute("tickets", tickets);
         return "tickets/index";
     }
     
