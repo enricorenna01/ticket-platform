cfea51515b43d26667f847f3f95df6ba7105368e
enricorenna01 <erenna39@gmail.com>
2025-03-26 15:25:05 +0100

    feat: added import sql (ticket)

---
+++ src/main/resources/templates/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tickets (title, description, created_at, updated_at, category_id, state_id, user_id) 
+VALUES 
+('Problema di login', 'L\'utente non riesce ad accedere al sistema', '2025-03-20', '2025-03-25', 1, 1, 2),
+('Errore 500', 'Pagina mostra errore 500 durante il caricamento', '2025-03-21', '2025-03-25', 2, 2, 3),
+('Richiesta nuovo accesso', 'Deve essere creato un nuovo account per il dipendente', '2025-03-22', '2025-03-25', 1, 3, 4),
+('Bug nella dashboard', 'Alcuni grafici non caricano correttamente', '2025-03-23', '2025-03-25', 3, 1, 5),
+('Reimpostazione password', 'L\'utente ha dimenticato la password e necessita di reset', '2025-03-24', '2025-03-25', 2, 2, 1);
